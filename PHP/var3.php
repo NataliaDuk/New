a033287d1deca69d066ace820010a4df4d3786ee
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2020-12-08 20:23:01 +0300

    var3

---
+++ PHP/var3.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $a=5;
+    echo $a . "<br>";
+    $a += 2;
+    // $a = $a + 2;
+    echo $a . "<br>";
+    $a -= 3;
+    echo $a . "<br>";
+    $a .= 4;
+    echo $a . "<br>";
+        
+    ?>
+</body>
+</html>
