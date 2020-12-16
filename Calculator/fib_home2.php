499b392eef365ae06af9f1d867d09f0689ee35f4
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2020-12-16 19:23:57 +0300

    fibonachi

---
+++ Calculator/fib_home2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fibonachi</title>
+</head>
+
+<body>
+    <?php
+    $F = 15;
+    $N1 = 0;
+    $N2 = 1;
+    do {
+        $i++;
+        $N = $N1 + $N2;
+        $N2 = $N1;
+        $N1 = $N;
+
+        echo $N . "<br>";
+    } while ($i < $F)
+    ?>
+</body>
+
+</html>
