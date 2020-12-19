89ebedff1318299128ee6083664ab90070d22fef
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2020-12-19 18:39:40 +0300

    correct

--- Calculator/index_arr.php
+++ Calculator/index_arr.php
@@ -4,9 +4,9 @@ $a = ["x"=>5, "y"=>7];
 // print_r($a);
 
 // echo $a[2];
-// $sum = 0;
-// for ($i = 0; $i < 5; $i++) {
-//     $sum = $sum + $a[$i];
-// }
-// echo $sum;
-print_r($a);
+$sum = 0;
+for ($i = 0; $i < 5; $i++) {
+    $sum = $sum + $a[$i];
+}
+echo $sum;
+// print_r($a);
