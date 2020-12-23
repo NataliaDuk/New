882e0b80bb381a4cf29f244645c15f3371747d80
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2020-12-23 19:15:03 +0300

    Create factopial.php

---
+++ Calculator/factopial.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    // $x = 1;
+    // $n = 10;
+    // echo $x . ", ";
+    // $n1 = $x * $x++;
+    // echo $n1. ", ";
+    $x=1;
+    $n=5;
+    // for ($i = 0; $i <= $n; $i++) {
+        $n1 = $x * $x++; $n2=$n1 * ++$i; echo $n1. ", ";
+    // }
+    // for (;;) {
+    //     if ($i > 10) {
+    //         break;
+    //     }
+    //     echo $i;
+    //     $i++;
+    // }
+
+    ?>
+
+
+
+
+</body>
+
+</html>
