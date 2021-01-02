9a56fc5e2138d7b8f347036cd228b7415019274e
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-02 17:37:58 +0300

    Create numbers_propis.php

---
+++ Calculator/numbers_propis.php
@@ -0,0 +1,52 @@
+<!-- <?php
+        if (!empty($_POST)) {
+            $name = $_POST['text'];
+        }
+        ?> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="post">
+        <p> Введите сообщение</p>
+        <p><textarea name="text"></textarea></p>
+        <input type="submit" value="Отправить">
+    </form><br>
+
+    <?php
+
+
+    $name = $_POST["text"];
+    print_r($name);
+    if (is_int($name)); {
+        $numbers = [
+            1 => "один",
+            2 => "два",
+            3 => "три",
+            4 => "четыре"
+
+        ];
+
+        foreach (str_split($name) as $value) {
+            echo " " . $numbers[$value];
+        }
+    }
+  
+
+    // echo $name=str_replace($numbers, $numbers[$value], $name);
+
+    ?>
+
+
+
+
+
+</body>
+
+</html>
