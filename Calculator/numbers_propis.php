b7f88a3b3c40a27bfbbf6a8a4207a4de3a668069
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-08 08:57:54 +0300

    correct1

--- Calculator/numbers_propis.php
+++ Calculator/numbers_propis.php
@@ -5,9 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style75.css">
 </head>
 
 <body>
+    <nav class="meny">
+        <a href="test_PHP_home.html">Главная</a>
+        <a href="#">Справочник PHP</a>
+        <a href="#">Курсы-IT</a>
+    </nav>
+    <h1>Сервис, заменяющий в тексте пользователя все цифры на слова прописи этих цифр.</h1>
     <form method="post">
         <p> Введите сообщение</p>
         <p><textarea name="text"></textarea></p>
@@ -18,14 +25,7 @@
 
     $name = $_POST["text"];
 
-    $numbers = [
-        0 => "ноль",
-        1 => "один",
-        2 => "два",
-        3 => "три",
-        4 => "четыре"
-
-    ];
+    $numbers = ["ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"];
     $numbers1 = str_split($name);
     foreach ($numbers1  as $value) {
         if (is_numeric($value)) {
