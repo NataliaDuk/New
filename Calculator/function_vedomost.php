33dcc6c8ac562f81b73a7d3b20687ed173330b42
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-14 20:24:48 +0300

    correct1

--- Calculator/function_vedomost.php
+++ Calculator/function_vedomost.php
@@ -8,32 +8,37 @@
 </head>
 
 <body>
-    <?php
+    <h1>Зарплатная ведомость</h1>
+    <table border="2"  width="800px">
+        <tr>
+            <th>N</th>
+            <th>Фамилия</th>
+            <th>Сумма, бел.руб.</th>
+        </tr>
 
-    $vedomost1 = [
-        [1, "Иванов", 500],
-        [2, "Петров", 600],
-        [3, "Сидоров", 700]
-    ];
-    //  print_r($vedomost);
+        <?php
 
-    function arrayName($vedomost)
-    {
-        $table = "<table>";
-        foreach ($vedomost as $value) {
-            echo "<td></td>";
-            for ($i = 0; $i <= count($value) - 1; $i++) {
-                echo "<tr>";
-                foreach ($vedomost as $value) {
-                    echo "<td>$value[$i]</td>";
-                }
+        $vedomost1 = [
+            [1, "Иванов", 1550],
+            [2, "Петров", 1600],
+            [3, "Сидоров", 1650],
+            [4, "Фролов", 1700],
+        ];
+
+        function arrayName($vedomost)
+
+        {
+            foreach ($vedomost as list($a, $b, $c)) {
+                $html = "<tr>";
+                echo "<td>$a</td>";
+                echo "<td>$b</td>";
+                echo "<td>$c</td>";
                 echo "</tr>";
             }
+            return $html;
         }
 
-        return $table;
-    }
+        echo arrayName($vedomost1);
 
-    echo arrayName($vedomost1);
-
-    ?>
+        ?>
+    </table>
