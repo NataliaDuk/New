8852f6b9ee64411f7973ec81d107d1d8f40c7db1
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-18 18:29:37 +0300

    correct2

--- Calculator/OPROS_HOME/file10.php
+++ Calculator/OPROS_HOME/file10.php
@@ -16,11 +16,13 @@ $data =  file("opros.txt");
 $N1 = $_POST["N1"];
 // echo $N1;
 $data[$N1] = ($data[$N1]+1)."\n";
+echo "<div class=meny>";
 echo "<h2>Результаты голосования: "; 
 echo "<h2>Очень хороший: " . $data[0] . "<h2>";
 echo "<h2>Хороший: " . $data[1]. "<h2>";
 echo "<h2>Нормальный: " . $data[2] . "<h2>";
 echo "<h2>Плохой: " . $data[3] . "<h2>";
+echo "</div>";
 
 $saveData = $data[0] . $data[1] . $data[2] . $data[3];
 $fp = fopen("opros.txt", 'w+');
