f41d1ebdb4600c64385b10146be509cbf032b0a9
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-20 21:44:43 +0300

    create opros_home2

---
+++ Calculator/OPROS_HOME 2/file10.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+    <link rel="stylesheet" href="style26.css">
+</head>
+
+<body>
+<?php
+
+
+// print_r($data);
+$N1 = $_POST["N1"];
+// echo $N1;
+$data =  file_get_contents("opros.txt");
+
+$data[$N1] = ($data[$N1]+1)."\n";
+
+
+echo "<div class=meny>";
+echo "<h2>Результаты голосования: "; 
+echo "<h2>Очень хороший: " . $data[0] . "<h2>";
+echo "<h2>Хороший: " . $data[1]. "<h2>";
+echo "<h2>Нормальный: " . $data[2] . "<h2>";
+echo "<h2>Плохой: " . $data[3] . "<h2>";
+echo "</div>";
+
+
+$saveData = $data[0] . $data[1] . $data[2] . $data[3];
+file_put_contents("opros.txt", $saveData);
+
+?>
+</body>
+
+</html>
