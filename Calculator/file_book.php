6d3ab47c35736581397565314afb3a6431fb6634
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-28 20:53:09 +0300

    home25

--- Calculator/file_book.php
+++ Calculator/file_book.php
@@ -1,34 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Book</title>
-   
-</head>
-
-<body>
 <?php
-$data =  file("forma.txt");
+    if (!empty($_POST)) {
+        $row = "\n<----->\n" .
+            $_POST["name"] . "\n" .
+            $_POST["email"] . "\n" .
+            $_POST["text"];
+        file_put_contents("file_guest.txt", $row, FILE_APPEND);
+        header("Location: ?");
+        die();
+    }
+  
+            $error = "";
+     $actionName = $_POST["name"];
 
+  if (!empty($actionName)) 
+  {
+    $name = trim($name);
+   
+      
+      if (empty($name)) // если не введено имя
+      {
+        $actionName = "";
+        $error = $error."Вы не ввели имя\n";
+      }
+  }
+      ?>
+    
+    
+    <!DOCTYPE html>
+    <html lang="en">
 
-$N1 = $_POST["N1"];
-// echo $N1;
-$data[$N1] = ($data[$N1]+1)."\n";
-echo "<div class=meny>";
-echo "<h2>Результаты голосования: "; 
-echo "<h2>Очень хороший: " . $data[0] . "<h2>";
-echo "<h2>Хороший: " . $data[1]. "<h2>";
-echo "<h2>Нормальный: " . $data[2] . "<h2>";
-echo "<h2>Плохой: " . $data[3] . "<h2>";
-echo "</div>";
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="style265.css">
+    </head>
 
-$saveData = $data[0] . $data[1] . $data[2] . $data[3];
-$fp = fopen("opros.txt", 'w+');
-fwrite($fp, $saveData);
-fclose($fp);
-?>
-</body>
+    <body>
+    
+            <h1>Гостевая книга</h1>
+            <div class="text">
+            <?php
 
-</html>
+            $data =file_get_contents("file_guest.txt");
+            $records = explode("<----->", $data);
+            echo "<table border='1'>";
+            foreach ($records as $record) {
+                $row = explode("\n", trim($record));
+                // print_r($row);
+                echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+            }
+            echo "</table>";
+
+            ?>
+            </div>
+            <div class=ident>
+                <form action="?" method="POST">
+                    <h2>Введите Ваше имя</h2>
+                    <input type="text" name="name">
+                    <h2>Введите адрес электронной почты</h2>
+                    <input type="text" name="email">
+                    <h2>Введите сообщение</h2>
+                    <input type="text" name="text">
+                    <input type="submit" value="ok">
+                </form>
+            </div>
+            
+    </body>
+
+    </html>
