92f13684cc7073b1eedc72807302a4464c7110f1
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-29 21:21:34 +0300

    lesson26 plus

--- Calculator/lesson26/file8.php
+++ Calculator/lesson26/file8.php
@@ -17,9 +17,11 @@
     $year = $date[0];
     $month = $date[1];
     $day = $date[2];
-    $second = time() - mktime(0, 0, 0, $month, $day, $year);
-    $days = floor($second / (60 * 60 * 24));
-    echo $days . " полных суток";
+
+  
+    $n = date("w", mktime(0,0,0,date("m"),date("d"),date("Y")));
+    echo  $n;
+
     ?>
 </body>
 
