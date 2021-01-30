371c6776beccacc7419a965aa73d61290ad181cd
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-01-30 12:04:01 +0300

    home26

--- Calculator/BOOK_GUEST_TIME/file_book.php
+++ Calculator/BOOK_GUEST_TIME/file_book.php
@@ -3,7 +3,8 @@
         $row = "\n<----->\n" .
             $_POST["name"] . "\n" .
             $_POST["email"] . "\n" .
-            $_POST["text"];
+            $_POST["text"]. "\n" .
+            $_POST["time"];
         file_put_contents("file_guest.txt", $row, FILE_APPEND);
         header("Location: ?");
         die(); 
@@ -20,28 +21,34 @@
     </head>
 
     <body>
-            <h1>Гостевая книга</h1>
+    <img src="7nSh.gif" alt="">
+    <h1>Гостевая книга</h1>
             <div class="text">
+            
             <?php
+            date("d.m.Y");
             $data =file_get_contents("file_guest.txt");
             $records = explode("<----->", $data);
             echo "<table border='1'>";
             foreach ($records as $record) {
                 $row = explode("\n", trim($record));
                 // print_r($row);
-                  echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
+                  echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td></tr>";
                   } 
             echo "</table>";
             ?>
             </div>
             <div class="ident">
+           
                 <form action="?" method="POST">
                     <h2>Введите Ваше имя</h2>
                     <input type="text" name="name" class="user">
                     <h2>Ваш e-mail</h2>
                     <input type="text" name="email" class="mail">
                     <h2>Ваш отзыв</h2>
-                    <input type="text" name="text" class="comment"><br>
+                    <input type="text" name="text" class="comment">
+                    <h2>Дата</h2>
+                    <input type="date" name="time">
                     <input type="submit" value="ok" class="input">  
                 </form>
             </div>
