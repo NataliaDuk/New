4e2e9dc548c6bcc02a886391752c239cfebdf4ee
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-06 12:01:21 +0300

    create2

--- Calculator/lesson27/12.php
+++ Calculator/lesson27/12.php
@@ -6,3 +6,10 @@ $pat="/Минск(.*?)\b/iu";
 $ret=file_get_contents('https://www.onliner.by/');
 echo $str1=preg_match_all($pat,$ret, $m);
 var_dump($m);
+
+<?php
+
+
+$ret=file_get_contents('TolstoyLev.txt');
+preg_match_all('/сударь\b/iu',$ret, $str);
+print_r($str);
