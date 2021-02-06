b7c97625543f6e2704cfef5f0a374c44a519ac76
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-06 16:19:37 +0300

    create3

--- Calculator/lesson27/12.php
+++ Calculator/lesson27/12.php
@@ -1,5 +1,5 @@
 <?php
-$str = $_POST["name"];
+// $str = $_POST["name"];
 
 $pat="/Минск(.*?)\b/iu";
 
@@ -7,9 +7,3 @@ $ret=file_get_contents('https://www.onliner.by/');
 echo $str1=preg_match_all($pat,$ret, $m);
 var_dump($m);
 
-<?php
-
-
-$ret=file_get_contents('TolstoyLev.txt');
-preg_match_all('/сударь\b/iu',$ret, $str);
-print_r($str);
--- Calculator/lesson27/17.php
+++ Calculator/lesson27/17.php
@@ -1,4 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $ret=file_get_contents('TolstoyLev.txt');
-preg_match_all('/сударь\b/iu',$ret, $str);
-print_r($str);
+// implode("<br>\r\n", $str);
+preg_match_all('/\bсударь\b/iu',$ret, $str);
+
+foreach ($str[0] as $key => $row) {
+	echo ($key + 1)  . '.' . $row;
+	if ($key < count($str[0]) - 1) {
+		echo "\n";
+	} 
+}
+preg_match_all('/\bсудар(.*?\[^ь])\b/iu',$ret, $str1);
+foreach ($str1[0] as $key => $row) {
+	echo ($key + 1)  . '.' . $row;
+	if ($key < count($str1[0]) - 1) {
+		echo "\n";
+	} 
+}
+?>
+</body>
+</html>
