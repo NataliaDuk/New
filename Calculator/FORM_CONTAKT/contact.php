c959b07b4e756d5f65e9b8f3b5d13648a29c0a53
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-11 21:21:01 +0300

    correct3

--- Calculator/FORM_CONTAKT/contact.php
+++ Calculator/FORM_CONTAKT/contact.php
@@ -1,92 +1,85 @@
 <?php
-    if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["text"] != "")) {
-        $row = "\n<----->\n" .
-            $_POST["tel"] . "\n" .
-            $_POST["email"] . "\n" .
-            $_POST["name"];
-            $_POST["url"];
-        file_put_contents("contact.txt", $row, FILE_APPEND);
-        header("Location: ?");
-        die(); 
+if ((!empty($_POST)) && ($_POST["name"] != "") && ($_POST["text"] != "")) {
+    $row = "\n<----->\n" .
+        $_POST["tel"] . "\n" .
+        $_POST["email"] . "\n" .
+        $_POST["name"];
+    $_POST["url"];
+    file_put_contents("contact.txt", $row, FILE_APPEND);
+    header("Location: ?");
+    die();
+}
+
+if (empty($_POST["tel"])) {
+    $errorTel = "Не введен номер телефона";
+} else {
+    $tel = $_POST["tel"];
+    if (!preg_match("/^\+\d[\d\(\)\-]{9,14}\d$/iu", $tel)) {
+        $errorTel = "Не корректно введен номер";
     }
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+}
 
-        if (empty($_POST["tel"])) {
-            $nameErr = "Name is required";
-        } else {
-            $name = test_input($_POST["tel"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "Only letters and white space allowed";
-                unset($name);
-            }  
-        }
-    
-        if (empty($_POST["email"])) {
-            $emailErr = "Email is required";
-        } else {
-            $email = test_input($_POST["email"]);
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "Invalid email format";
-                unset($email);
-            }
-        }
-        if (empty($_POST["name"])) {
-            $nameErr = "Name is required";
-        } else {
-            $name = test_input($_POST["name"]);
-            if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "Only letters and white space allowed";
-                unset($name);
-            }  
-        }
-    
-        if (empty($_POST["website"])) {
-            $websiteErr = "Website is required";
-        } else {
-            $website = test_input($_POST["website"]);
-            if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) {
-                $websiteErr = "Invalid URL";
-                unset($website);
-            }
-        }
-    
-      ?>
-    <!DOCTYPE html>
-    <html lang="en">
+if (empty($_POST["email"])) {
+    $errorMail = "Не введен Email";
+} else {
+    $email = $_POST["email"];
+    if (!preg_match("/^.+@.+\..+$/iu", $email)) {
+        $errorMail = "Не верный формат email";
+    }
+}
+if (empty($_POST["name"])) {
+    $errorName = "Не введено имя";
+} else {
+    $name = $_POST["name"];
+    if (!preg_match("/^[а-яa-zA-ZА-Я ]*$/iu", $name)) {
+        $errorName = "Не корректно введено имя";
+    }
+}
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Contact_fornm</title>
-        <link rel="stylesheet" href="style265.css">
-    </head>
+if (empty($_POST["url"])) {
+    $errorURL = "Не введен адрес соцсети";
+} else {
+    $url = $_POST["url"];
+    if (!preg_match("/\bhttps:\/\/vk\.com\/|www\.facebook\.com\/|www\.instagram\.com\/|twitter\.com\//", $url)) {
+        $errorURL = "Не корректно введен адрес";
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-    <body>
-            <h1>Форма</h1>
-            <div class="text">
-            <?php
-            $data =file_get_contents("contact.txt");
-            $records = explode("<----->", $data);
-            echo "<table border='1'>";
-            foreach ($records as $record) {
-                $row = explode("\n", trim($record));
-                // print_r($row);
-                  echo "<tr><td>$row[0]</td><td>$row[1]</td><td>$row[2]</td></tr>";
-                  } 
-            echo "</table>";
-            ?>
-            </div>
-            <div class="ident">
-                <form action="?" method="POST">
-                    <h2>Введите Ваше имя</h2>
-                    <input type="text" name="name" class="user">
-                    <h2>Ваш e-mail</h2>
-                    <input type="text" name="email" class="mail">
-                    <h2>Ваш отзыв</h2>
-                    <input type="text" name="text" class="comment"><br>
-                    <input type="submit" value="ok" class="input">  
-                </form>
-            </div>
-    </body>
-    </html>
-   
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact_fornm</title>
+    <link rel="stylesheet" href="style265.css">
+</head>
+
+<body>
+    <h1>Заполните контактные данные</h1>
+    <div class="text">
+        <?php
+        $data = file_get_contents("contact.txt");
+        $records = explode("<----->", $data);
+        ?>
+    </div>
+    <div class="ident">
+        <form action="?" method="POST">
+            <h2>Введите Ваше имя</h2>
+            <input type="text" name="name" class="user">
+            <span class="textError"> <?php echo $errorName; ?></span>
+            <h2>Введите Ваш номер телефона<br><i>(в международном формате +xxxxxxxxx)</i></h2>
+            <input type="text" name="tel" class="number">
+            <span class="textError"> <?php echo $errorTel; ?></span>
+            <h2>Ваш e-mail</h2>
+            <input type="text" name="email" class="mail">
+            <span class="textError"> <?php echo $errorMail; ?></span>
+            <h2>Ваш адрес в соцсети<br><i>(ВКонтакте, Facebook, Instagram, Twitter)</i></h2>
+            <input type="text" name="url" class="social">
+            <span class="textError"> <?php echo $errorURL; ?></span><br><br>
+            <input type="submit" value="ok" class="input">
+        </form>
+    </div>
+</body>
+
+</html>
