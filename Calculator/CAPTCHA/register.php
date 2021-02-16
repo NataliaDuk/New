d0c02a7779d32239d53abce881aee28f4f5ea614
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-16 15:55:15 +0300

    correct

--- Calculator/CAPTCHA/register.php
+++ Calculator/CAPTCHA/register.php
@@ -1,5 +1,6 @@
-<?php
-   echo $inputNumber=preg_replace(
+<?php   
+    session_start();
+    $inputNumber=preg_replace(
         [
             "/\bноль/iu",
             "/\bодин/iu",
@@ -26,38 +27,11 @@
         ],
         $_POST["captcha"]
     );
-    echo $inputNumber2=preg_replace('/[ ]/', '', $inputNumber);
-   
-    session_start();
+    $inputNumber2=preg_replace('/[ ]/', '', $inputNumber);
     $inputNumber2=$_POST["captcha"];//ответ пользователя
-        if ($inputNumber2==$_SESSION['captcha1']&&($captcha !="")) {
+        if ($inputNumber2==$_SESSION['rand_code']&&($inputNumber2 !="")) {
        echo "Капча введена правильно";
     } else {
         echo "Капча введена не правильно";
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style75.css">
-</head>
-
-<body>
-
-   
-    <form class="meny1" method="post" action="">
-    <h1>1.Введите код с картинки</h1>
-        <img src="captchaimg.php" alt="" />
-        <input type="text" name="captcha"><br><br>
-        <input type="submit" value="Регистрация"><br><br>
-      
-    </form>
-    
-    
-</body>
-
-</html>
--- captcha.php
+++ captcha.php
@@ -14,7 +14,7 @@
 	$white = imagecolorallocate($image, 255, 255, 255);
 
 	imagefilledrectangle($image,0,0,399,99,$white);
-	imagettftext ($image, 30, 0, 10, 40, $color, $dir."ArialRegular.ttf", $_SESSION['rand_code']);
+	imagettftext ($image, 30, 0, 10, 40, $color, $dir."verdana.ttf", $_SESSION['rand_code']);
 
 	header("Content-type: image/png");
 	imagepng($image);
