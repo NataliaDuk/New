16fb81d2d4091b0516779c65695593f1075acf43
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-23 13:02:52 +0300

    correct

--- Calculator/lesson32/guestbook/main.php
+++ Calculator/lesson32/guestbook/main.php
@@ -13,27 +13,9 @@
     include "function.php";
     $data = json_decode(file_get_contents("data.json"), true);
     foreach ($data as $row)  {if (!moderator($row["msg"]))
-       
         echo smail($row["msg"])."<br>". $row["name"] ."<br>".$row["time"] ."<br>\n";
-       
+        else echo "У нас не ругаются!";
     }
-    // echo months ($row["time"]) ."<br>\n";
-    // foreach ($data as $row1) {
-    //     echo months ($row1["time"]) ."<br>\n";
-       
-    // } 
-    // date(".md.Y");
-    // $months = [".01." => "января", ".02." => "февраля", 
-    // ".03." => "марта", ".04." => "апреля", ".05." => "мая", ".06." => "июня", 
-    // ".07." => "июля", ".08." => "августа", ".09." => "сентября",
-    // ".10." => "октября", ".11." => "ноября", ".12." => "декабря"];
-    // $m = date(".m."); //для замены
-    // $time=str_replace($m, " ".$months[$m]." ", $time);
-    // foreach ($data as $row) {
-    //     echo months ($row["msg"]). $time. $row["name"] . "<br>\n";
-    // }
-   
-
     ?>
     <form action="add.php" method="post">
         <textarea name="msg" cols="60" rows="5"></textarea><br>
