cac7f47a9a5d99d8d90cf72bf174b355147bc825
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-02-28 16:26:03 +0300

    correct

--- Calculator/lesson32/guestbook_table/main.php
+++ Calculator/lesson32/guestbook_table/main.php
@@ -1,52 +1,55 @@
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <title>Guetbook</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <title>Guetbook</title>
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<div class="container">
-        <div class="row">
-          <div class="col-sm">
-          </div>
-          <div class="col-6">
-         <table class="table table-bordered border-primary">
-        <tr class='table-primary'><th class='w-25 p-3'>Дата</th><th>Комментарий</th><th class='w-25 p-3'>Имя</th></tr>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+      </div>
+      <div class="col-6">
+        <table class="table table-bordered border-primary">
+          <tr class='table-primary'>
+            <th class='w-25 p-3'>Дата</th>
+            <th>Комментарий</th>
+            <th class='w-25 p-3'>Имя</th>
+          </tr>
           <?php
-          
-    include "function.php";
-    foreach (loadMessages() as $row) {
-        echo "<tr><td class='table-secondary'>".  dateFormat($row["date_time"])  ."</td><td>". smile($row["msg"]) . "</td><td>" . $row["name"] .
-  "</td></tr>";
-    }
-    
-    ?>
-  </table>
-</table>
-          <form action="add.php" method="post">
-     <div class="form-floating">
+          include "function.php";
+          foreach (loadMessages() as $row) {
+            echo "<tr><td class='table-secondary'>" .  dateFormat($row["date_time"])  . "</td><td>" . smile($row["msg"]) . "</td><td>" . $row["name"] .
+              "</td></tr>";
+          }
+          ?>
+        </table>
+        </table>
+        <form action="add.php" method="post">
+          <div class="form-floating">
             <textarea name="msg" class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
             <label for="floatingTextarea">Коментарий</label>
-        </div>
-        <div class="input-group flex-nowrap">
+          </div>
+          <div class="input-group flex-nowrap">
             <span class="input-group-text" id="addon-wrapping">Имя Фамилия</span>
             <input type="text" name="name" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
           </div>
-        <button type="submit" class="btn btn-primary">Отправить</button>
-    </form>
-          </div>
-          <div class="col-sm">
-            
-          </div>
-        </div>
+          <button type="submit" class="btn btn-primary">Отправить</button>
+        </form>
       </div>
-   
+      <div class="col-sm">
+
+      </div>
+    </div>
+  </div>
+
 </body>
+
 </html>
