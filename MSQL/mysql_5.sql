8e8e609139a1203627ec70076e0b663e3b97a3da
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-03-21 10:05:36 +0300

    continued

--- MSQL/mysql_5.sql
+++ MSQL/mysql_5.sql
@@ -2,3 +2,32 @@
 SELECT `SNAME`
 FROM `salespeople`, `orders`
 WHERE `salespeople`.`SNUM`=`orders`.`SNUM` AND `orders`.`ODATE`="1990-06-10"
+-- подсчитать количество продавцов, которые работали с клиентами с самым низким рейтингом
+SELECT
+    COUNT(DISTINCT `SNAME`) AS "Количество продавцов"
+FROM
+    `salespeople`,
+    `customers`,
+    `orders`
+WHERE
+        `orders`.`CNUM` = `customers`.`CNUM` AND `orders`.`SNUM` = `salespeople`.`SNUM` AND `RATING` =(
+    SELECT
+        MIN(`RATING`)
+    FROM
+        `customers`
+)
+-- найти города, в которых больше двух клиентов
+SELECT `CITY`
+FROM `customers`
+GROUP BY `CITY`
+HAVING COUNT(`CNAME`)>2
+-- найти города, сумма заказов из которых больше,чем средняя сумма заказов за 10/03/1990
+SELECT
+    `CITY`
+FROM
+    `customers`,
+    `orders`
+WHERE
+        `customers` = `CNUM` AND `orders` = `CNUM` AND AVG(`AMT`)=(SELECT AVG(`AMT`) FROM `orders` WHERE `ODATE`='1990-10-03')
+GROUP BY `CITY`
+HAVING SUM(`AMT`)>AVG(`AMT`)
