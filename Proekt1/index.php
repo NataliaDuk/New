41b775fde8316c1509d8a88c1b87b8e525c30ab6
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-04-12 17:44:02 +0300

    proekt1

---
+++ Proekt1/index.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" real="stylesheet">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<h1>Hello, world!</h1>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-3 col-md-3 col-lg-3">
+
+        </div>
+        <div class="col-sm-6 col-md-6 col-lg-6">
+
+        </div>
+        <div class="col-sm-3 col-md-3 col-lg-3">
+
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+    <div class="col-sm-6 col-md-6 col-lg-6">
+
+    </div>
+    <div class="col-sm-3 col-md-3 col-lg-3">
+
+    </div>
+</div>
+</div>
+<!-- Optional JavaScript; choose one of the two! -->
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="js/jquery-3.6.0.min.js"></script>
+
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
