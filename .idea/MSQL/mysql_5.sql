912005a67abd0edff9442c9a719819d861d1bf82
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-04-16 14:56:53 +0300

    home table

---
+++ .idea/MSQL/mysql_5.sql
@@ -0,0 +1,42 @@
+-- найти продавцов, которые обслуживали заказ 10/06/1990
+SELECT `SNAME`
+FROM `salespeople`, `orders`
+WHERE `salespeople`.`SNUM`=`orders`.`SNUM` AND `orders`.`ODATE`="1990-06-10"
+-- подсчитать количество продавцов, которые работали с клиентами с самым низким рейтингом
+SELECT
+    COUNT(DISTINCT `SNAME`) AS "Количество продавцов"
+FROM
+    `salespeople`,
+    `customers`,
+    `orders`
+WHERE
+        `orders`.`CNUM` = `customers`.`CNUM` AND `orders`.`SNUM` = `salespeople`.`SNUM` AND `RATING` =(
+    SELECT
+        MIN(`RATING`)
+    FROM
+        `customers`
+)
+-- найти города, в которых больше двух клиентов
+SELECT `CITY`
+FROM `customers`
+GROUP BY `CITY`
+HAVING COUNT(`CNAME`)>2
+-- найти города, сумма заказов из которых больше,чем средняя сумма заказов за 10/03/1990
+SELECT
+    `customers`.`CITY`
+FROM
+    `customers`,
+    `orders`
+WHERE
+    `customers`.`CNUM` = `orders`.`CNUM`
+GROUP BY
+    `customers`.`CITY`
+HAVING
+    SUM(`orders`.`AMT`) >(
+    SELECT
+        AVG(`AMT`)
+    FROM
+        `orders`
+    WHERE
+        `ODATE` = '1990-03-10'
+)
