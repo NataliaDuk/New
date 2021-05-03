1441a2e2f18ab446815e911ccfc75b5c6cd95308
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-05-03 20:29:37 +0300

    popravka home List

--- OOP/List/run.php
+++ OOP/List/run.php
@@ -3,24 +3,12 @@ include "autoLoad.php";
 
 $ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
 echo $ol->setType("1")->html();
-
-$ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
 echo $ol->setType("A")->html();
-
-$ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
 echo $ol->setType("a")->html();
-
-$ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
 echo $ol->setType("I")->html();
-
-$ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
 echo $ol->setType("i")->html();
 
-$ul= new Ul(['Иванов', 'Петров', 'Сидоров']);
+$ul= new Ul(['Фролов', 'Галкин', 'Воронов']);
 echo $ul->setType("disk")->html();
-
-$ul= new Ul(['Иванов', 'Петров', 'Сидоров']);
 echo $ul->setType("circle")->html();
-
-$ul= new Ul(['Иванов', 'Петров', 'Сидоров']);
 echo $ul->setType("square")->html();
