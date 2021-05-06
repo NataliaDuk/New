1642b72d3f2f9a7b70534f8629192aca7a1b398a
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-05-06 18:29:05 +0300

    home list

--- OOP/List/run.php
+++ OOP/List/run.php
@@ -2,6 +2,7 @@
 include "autoLoad.php";
 
 $ol= new Ol(['Иванов', 'Петров', 'Сидоров']);
+
 echo $ol->setType("1")->html();
 echo $ol->setType("A")->html();
 echo $ol->setType("a")->html();
@@ -9,9 +10,10 @@ echo $ol->setType("I")->html();
 echo $ol->setType("i")->html();
 
 $ul= new Ul(['Фролов', 'Галкин', 'Воронов']);
+
 echo $ul->setType("disk")->html();
 echo $ul->setType("circle")->html();
 echo $ul->setType("square")->html();
-
-echo get_class($ol->setData([]));
-echo get_class($ul->setData([]));
+//
+//echo get_class($ol->setData([]));
+//echo get_class($ul->setData([]));
