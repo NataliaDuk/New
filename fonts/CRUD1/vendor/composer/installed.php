84645de61991e68770f908dc867edc5cf054c6b0
NataliaDuk <74614637+NataliaDuk@users.noreply.github.com>
2021-06-08 21:43:10 +0300

    create NEW CRUD1

---
+++ fonts/CRUD1/vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'w1020/html' => 
+    array (
+      'pretty_version' => 'v0.0.5',
+      'version' => '0.0.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4d8889bccda6cff74ab6e9119ecaef5c8c9a7904',
+    ),
+    'w1020/orm' => 
+    array (
+      'pretty_version' => 'v2.1.7',
+      'version' => '2.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'dd44969121bf3ddad2edb481dd463d742ab1a49e',
+    ),
+  ),
+);
